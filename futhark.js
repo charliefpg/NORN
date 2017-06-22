a479d956bd47e22e72aaf9cc5a21acdc6033e78d
Carlos Pedraza <carlos.f.pedraza88@gmail.com>
2017-06-22 16:53:38 -0300

    futhark
    The files containing the strings for each rune. It will get the text from this file and print it on the browser.

---
+++ futhark.js
@@ -0,0 +1,11 @@
+var Fehu = "ᚠ mobile wealth, also livestock; abundance and financial strength";
+
+var Uruz = "ᚢ auroch, a wild ox; physical strength and speed, great energy  and health";
+
+var Thurisaz = "ᚦ Thor, or a giant;";
+
+var Ansuz = "ᚨ The Aesir Gods";
+
+var Raido = "ᚱ Journey, to ride";
+
+var Kaun = "ᚲ Ulcer"
